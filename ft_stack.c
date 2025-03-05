8042a091f9da3217c31dafd8e1a3e5aba9d92618
youssef <youssef.rhandoumi@gmail.com>
2025-03-05 17:42:48 +0000

    fixed norm errors

--- ft_stack.c
+++ ft_stack.c
@@ -6,16 +6,16 @@
 /*   By: yrhandou <yrhandou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/01 10:59:25 by yrhandou          #+#    #+#             */
-/*   Updated: 2025/03/02 12:24:53 by yrhandou         ###   ########.fr       */
+/*   Updated: 2025/03/05 17:39:15 by yrhandou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PushSwap.h"
-#include "utils/libft.h"
+#include "coreutils/libft.h"
 
-void print_stacks_side_by_side(t_stack *stack1, t_stack *stack2)
+void	print_stacks_side_by_side(t_stack *stack1, t_stack *stack2)
 {
-	while (stack1 != NULL || stack2 != NULL)
+	while ((stack1 != NULL) || (stack2 != NULL))
 	{
 		if (stack1 != NULL)
 		{
@@ -39,7 +39,7 @@ void print_stacks_side_by_side(t_stack *stack1, t_stack *stack2)
 	ft_putstr_fd(RESET "", 1);
 }
 
-void print_stack(t_stack *stack)
+void	print_stack(t_stack *stack)
 {
 	ft_printf(BLU "|     |\n");
 	while (stack)
@@ -50,28 +50,30 @@ void print_stack(t_stack *stack)
 	ft_printf(BLU "-------\n");
 	ft_printf(BLK "Stack A\n");
 }
-t_stack *create_node(int data)
+
+t_stack	*create_node(int data)
 {
-	t_stack *new_node;
+	t_stack	*new_node;
 
 	new_node = (t_stack *)malloc(sizeof(t_stack));
 	if (!new_node)
-		return NULL;
+		return (NULL);
 	new_node->data = data;
 	new_node->next = NULL;
-	return new_node;
+	return (new_node);
 }
-void push_node(t_stack **head, int data)
+
+void	push_node(t_stack **head, int data)
 {
-	t_stack *new_node;
+	t_stack	*new_node;
 
 	new_node = create_node(data);
 	if (!new_node)
-		return;
+		return ;
 	if (!(*head))
 	{
 		*head = new_node;
-		return;
+		return ;
 	}
 	new_node->next = *head;
 	*head = new_node;
